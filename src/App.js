babacb2a8081acd6bc0917c2541b70853b2ef807
sophiaalexis1 <122618542+sophiaalexis1@users.noreply.github.com>
2023-08-08 18:15:57 -0400

    created footer

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import { useState } from "react";
 import "./App.css";
 import data from './data/data.json';
 import NavigationBar from "./Components/NavBar/NavBar";
+import Footer from "./Components/Footer/Footer";
 
 function App() {
   console.log(data);
@@ -9,6 +10,7 @@ function App() {
   return (
     <div>
       <NavigationBar/>
+      <Footer/>
     </div>
   );
 }
