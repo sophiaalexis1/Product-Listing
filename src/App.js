9a74f3ea48c3f03dcb11a6545cb24b56eaaa0594
sophiaalexis1 <122618542+sophiaalexis1@users.noreply.github.com>
2023-08-12 19:00:21 -0400

    created and added the CategorySelect component

--- src/App.js
+++ src/App.js
@@ -1,20 +1,34 @@
 import { useState } from "react";
 import Data from '../src/data/data.json'
 import "./App.css";
-import Product from './Components/Product/Product'
 import ProductList from "./Components/ProductList/ProductList";
 import NavigationBar from "./Components/NavBar/NavBar";
 import Footer from "./Components/Footer/Footer";
+import CategorySelect from "./Components/CategorySelect/CategorySelect";
 
 const App = () => {
   const [products, setProducts] = useState(Data.products);
+  const [selectedCategory, setSelectedCategory] = useState("");
   
+  const handleSelectedCategory = (category) => {
+    setSelectedCategory(category);
+  };
+
+  const filteredProducts = selectedCategory
+    ? products.filter((product) => product.category === selectedCategory)
+    : products; 
+
   return (
     <div>
       <NavigationBar/>
       <div className="app">
         <h1>Product Listing</h1>
-      <ProductList products={products} />
+      <CategorySelect 
+        products={products}
+        selectedCategory={selectedCategory}
+        onSelectCategory={handleSelectedCategory}
+      />
+      <ProductList products={filteredProducts} />
       </div>
       <Footer/>
     </div>
---
+++ src/Components/CategorySelect/CategorySelect.jsx
@@ -0,0 +1,25 @@
+import { useState } from "react";
+import Product from '../Product/Product'
+
+const CategorySelect = ({products, selectedCategory, onSelectCategory}) => {
+    const allCategories = [...new Set(products.map((product) => product.category))];
+    return (
+        <div className="category-select">
+            <label htmlFor="category">Select Category:</label>
+            <select
+                id="category"
+                value={selectedCategory}
+                onChange={(e) => onSelectCategory(e.target.value)}
+            >
+            <option value="">All Products</option>
+            {allCategories.map((category) => (
+                <option key={category} value={category}>
+                    {category}
+                </option>
+          ))}
+            </select>
+        </div>
+    );
+};
+
+export default CategorySelect;
