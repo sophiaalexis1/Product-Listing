c13338127dfedb00ea5e5af2cf9b315eb99dfb48
sophiaalexis1 <122618542+sophiaalexis1@users.noreply.github.com>
2023-08-12 20:00:49 -0400

    added stars for ratings in product

--- src/Components/Product/Product.jsx
+++ src/Components/Product/Product.jsx
@@ -2,6 +2,30 @@ import React from 'react';
 import './Product.css'
 
 const Product = ({ product }) => {
+  const renderRatingStars = (rating) => {
+    const maxRating = 5;
+    const fullStars = Math.floor(rating);
+    const halfStar = rating - fullStars >= 0.5;
+
+    const stars = [];
+
+    for (let i = 0; i < fullStars; i++) {
+      stars.push(<i key={i} className="fas fa-star"></i>);
+    }
+
+    if (halfStar) {
+      stars.push(<i key={fullStars} className="fas fa-star-half-alt"></i>);
+    }
+
+    const emptyStars = maxRating - stars.length;
+
+    for (let i = 0; i < emptyStars; i++) {
+      stars.push(<i key={`empty-${i}`} className="far fa-star"></i>);
+    }
+
+    return stars;
+  };
+
   return (
     <div className="product">
       <img src={product.thumbnail} alt={product.title} />
@@ -9,7 +33,7 @@ const Product = ({ product }) => {
       <p>Brand: {product.brand}</p>
       <p>Category: {product.category}</p>
       <p>Quantity in Stock: {product.stock}</p>
-      <p>Rating: {product.rating} out of 5</p>
+      <div className="rating">{renderRatingStars(product.rating)}</div>
     </div>
   );
 };
